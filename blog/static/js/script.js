db434055b23c3eb9273763e4cc2d7a8a8ee7f8ba
lauriss <lavrencij@inbox.lv>
2022-07-07 23:20:29 +0300

    Removed all inline styles, changed headers font, removed some BS classes

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -1,21 +1,20 @@
-const arrow = document.getElementById("arrow");
-
-if (arrow) {
-	arrow.style.display = "none";
-}
-
 window.onscroll = function () {
-	scrollFunction();
+  scrollFunction();
 };
 
+const arrow = document.getElementById("arrow");
+
 function scrollFunction() {
-if (arrow) {
-	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-			// End value
-			arrow.style.display = "block";
-		} else {
-			// Start value
-			arrow.style.display = "none";
-		}
-	}
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    // End value
+    arrow.style.display = "block";
+  } else {
+    // Start value
+    arrow.style.display = "none";
+  }
 }
+
+[...document.querySelectorAll('[data-bs-toggle="tooltip"]')]
+.forEach(el => new bootstrap.Tooltip(el))
+
+hljs.initHighlightingOnLoad();
