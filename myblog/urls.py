d6fa176592f2e83e270a0ba4fea50cce8f662d8d
LaSTiqq <lavrencij13@gmail.com>
2023-08-29 13:38:59 +0300

    Reworked website idea

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,8 +5,7 @@ app_name = 'myblog'
 
 urlpatterns = [
     path('', MainView.as_view(), name='home'),
-	path('blog/', BlogList.as_view(), name='blog'),
-	path('blog/<slug:slug>', BlogDetail.as_view(), name='blog_detail'),
-	path('other/', Other.as_view(), name='other'),
-	path('tag/<int:tag_id>', BlogByTag.as_view(), name='tag'),
+    path('blog/', BlogList.as_view(), name='blog'),
+    path('blog/<slug:slug>', BlogDetail.as_view(), name='blog_detail'),
+    path('tag/<int:tag_id>', BlogByTag.as_view(), name='tag'),
 ]
